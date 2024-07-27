2db5654a95896e86a8af9e0ca5513cc0c1482391
Ali AI <aliai@Alis-MacBook-Pro.local>
2024-07-26 21:54:07 -0500

    Bug fixes

--- hshacks/pages/components/historypage_component/landing/HistoryLanding.tsx
+++ hshacks/pages/components/historypage_component/landing/HistoryLanding.tsx
@@ -2,21 +2,21 @@ import React from 'react'
 import Button from '../../global/button/Button'
 
 const HistoryLanding = () => {
-    return (
-        <div className='text-white h-screen background'>
-            <div>
-                <div className='text-center flex flex-col gap-10 h-screen items-center justify-center'>
-                    <div className='flex flex-col gap-3'>
+    return ( 
+          <div className='background'>
+          <div className='text-white flex flex-col items-center justify-between h-screen gap-10'> 
+                  <div className='text-center h-screen flex flex-col gap-10 items-center pt-20 justify-center px-4 md:px-8 lg:px-16' style={{minHeight: "300px"}}>
+                  <div className='flex flex-col gap-3'>
                         <h1 className='text-6xl font-extrabold'><span className="HSBlue">HSHacks</span> 2024</h1> 
                     </div> 
-                </div>
-                <div className='absolute w-full bottom-10 text-white flex justify-center'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-9 animate-bounce">
+                  </div>
+                  <div className='w-full text-white flex justify-center pb-10' style={{opacity: 0.8}}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-9 animate-bounce">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5" />
                     </svg>
-                </div>
-            </div>
-        </div>
+              </div>
+              </div>
+          </div>
     )
 }
 
