68a65481b17e66c4444d735bc27a4fafd55b3d35
Ali Alchikh Ibrahim <alialchi07@gmail.com>
2024-09-16 19:29:33 -0500

    Quick changes

--- hshacks/pages/components/homepage_component/faq/FAQ.tsx
+++ hshacks/pages/components/homepage_component/faq/FAQ.tsx
@@ -11,7 +11,7 @@ const FAQ = () => {
         </div> 
         <div className='flex gap-3 w-full flex-col items-center justify-center pt-10'>
             <FAQBlock question="What do I do if registeration is over?" answer="If you miss the registeration process, then you can contact our team at team@hshacks.org."/>
-            <FAQBlock question="What do I need to bring to HSHacks?" answer="You aren't requried to bring anything besides an ID! We provide food and computers, but we highly recommend bringing your own computer, if you're able to, since we might run out. If you have any allergies, we recommend bringing a lunch in case our allergy-free lunches run out."/>
+            <FAQBlock question="What do I need to bring to HSHacks?" answer="All you need to bring is a computer and your ID; we'll provide everything else."/>
             <FAQBlock question="Is there an age limit for HSHacks?" answer="HSHacks is only open to high schoolers so unfortunately, if you are not in grades 9-12, we can't offer you a place as a participant. Contact team@hshacks.org for any questions."/>
             <FAQBlock question="How do I check-in at HSHacks?" answer="To check-in, simply give us your name and show us an ID when you enter."/>
             <FAQBlock question="What if I don't have an ID?" answer="Everyone is required to have a state or school ID. If you don't have an ID, have a parent/guardian check you in using theirs."/>
