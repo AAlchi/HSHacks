ee6a0f4466627a8662e486316adda6961826d8b9
AAlchi <alialchi07@gmail.com>
2025-04-03 21:51:25 -0500

    Quick fix

--- hshacks/pages/index.tsx
+++ hshacks/pages/index.tsx
@@ -6,7 +6,6 @@ import Landing from "./components/homepage_component/landing/Landing";
 import Team from "./components/homepage_component/team/Team";
 import Sponsors from "./components/homepage_component/sponsors/Sponsors";
 import Head from "next/head";
-import Schedule from "./components/homepage_component/schedule/Schedule";
 
 export default function Home() {
   return (
