179ff9952e10eaeba55addba8c4f46a60de8af50
AAlchi <alialchi07@gmail.com>
2025-04-13 16:37:47 -0500

    Bugs fixed

--- hshacks/pages/components/homepage_component/landing/Landing.tsx
+++ hshacks/pages/components/homepage_component/landing/Landing.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Button from '../../global/button/Button' 
 import { motion } from "framer-motion";
 
