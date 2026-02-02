5861dd60efac49d621df49c5892d06d764cc713f
AAlchi <alialchi07@gmail.com>
2026-02-02 17:20:44 -0600

    Registration open

--- hshacks/pages/components/registeration_component/landing/RegisterationLanding.tsx
+++ hshacks/pages/components/registeration_component/landing/RegisterationLanding.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React from 'react'
+import Button from '../../global/button/Button'
 
 const RegisterationLanding = () => {
     // if (typeof document !== 'undefined') { 
@@ -18,9 +19,10 @@ const RegisterationLanding = () => {
                         <h1 className='text-5xl font-extrabold one'>
                             <span className="HSBlue">HSHacks <span className='text-white'>Registeration</span></span>
                         </h1>
-                        <p className='text-lg font-bold two px-3'>
-                            Registeration Opens Soon
-                        </p>
+                        <br></br>
+                        <div className='flex justify-center'>
+                            <Button name="Registration Open!" navigate="https://forms.gle/h2RWY6wS49BdwWmXA" color="bg-white text-black" icon="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 8.25.22-.22a.75.75 0 0 1 1.28.53v6.441c0 .472.214.934.64 1.137a3.75 3.75 0 0 0 4.994-1.77c.205-.428-.152-.868-.627-.868h-.507m-6-2.25h7.5M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                        </div>                        
                     </div>
                 </div>
                 <div className='w-full bg-white text-black flex justify-evenly md:flex-row flex-col' style={{ opacity: 0.8, flex: "0.03" }}>
